fc10edbb7c03ab6be6eff98c83e9da6d5b1bce7e
Tomas Sidenfaden <trsidenfaden@yahoo.com>
2018-08-14 09:40:30 -0700

    Add navigation controller embed ItemListViewController inside

--- ToDoTests/StoryboardTests.swift
+++ ToDoTests/StoryboardTests.swift
@@ -21,4 +21,12 @@ class StoryboardTests: XCTestCase {
         super.tearDown()
     }
     
+    func test_InitialViewController_IsItemListViewController() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let navigationController =
+            storyboard.instantiateInitialViewController() as! UINavigationController
+        let rootViewController = navigationController.viewControllers[0]
+        XCTAssertTrue(rootViewController is ItemListViewController)
+    }
+    
 }
